6ee46b4e4370b16b799993103d1501d457e02975
1272391326@qq.com <1272391326@qq.com>
2024-09-21 14:24:44 +0800

    111

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.less"
+
 import { ConfigProvider } from "antd"
 import App from "./App.jsx"
 import zhCN from "antd/locale/zh_CN"
+
+import "./index.less"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
